ce4d0d112f5e58c56771e02d36c26180970cdb8a
Matt Williams <matthew.steven.williams@gmail.com>
2024-11-23 10:35:52 +0100

    create POST route

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,26 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+# Utility functions
+def validate_post_data(data):
+    if not data.get("title") or not data.get("content"):
+        return False
+    return True
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    return jsonify(POSTS)
+    if request.method == 'POST':
+        new_post = request.get_json()
+        if not validate_post_data(new_post):
+            return jsonify({"error": "Invalid post"}), 400
+
+        new_id = max(post['id'] for post in POSTS) + 1
+        new_post['id'] = new_id
+
+        POSTS.append(new_post)
+
+    return jsonify(POSTS), 201
 
 
 if __name__ == '__main__':
